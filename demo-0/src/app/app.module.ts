517b4a246151c4c3e6ea710c31517718d4b57893
somi11 <somisun41@gmail.com>
2025-01-14 09:51:19 +0500

    actions added and effects added

--- demo-0/src/app/app.module.ts
+++ demo-0/src/app/app.module.ts
@@ -25,7 +25,7 @@ import { EffectsModule } from '@ngrx/effects';
     maxAge : 25,
     logOnly: environment.production
     }),
-    EffectsModule.forRoot([])
+    EffectsModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent],
